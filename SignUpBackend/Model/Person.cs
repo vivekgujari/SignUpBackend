e8624eca236dc39514cd6c1148a3597bd75c3c77
Vivek Gujari <vivekgujari@gmail.com>
2023-03-31 20:55:33 -0600

    added crud apis and db connection

---
+++ SignUpBackend/Model/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignUpBackend.Model
+{
+    public class Person
+    {
+        [Required]
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
